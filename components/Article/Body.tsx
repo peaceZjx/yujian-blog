722807aa9be21804cbb6701e26e05cea960f3814
coder ZJX <zjx@yuxisoft.cn>
2020-05-11 16:42:31 +0800

    修改

--- components/Article/Body.tsx
+++ components/Article/Body.tsx
@@ -3,44 +3,7 @@ import ReactMarkdown from 'react-markdown';
 import Code from '../Code';
 import styles from './styles.less';
 
-let markdown =
-  '# 这是标题\n' +
-  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
-  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
-  '**这是加粗的文字**\n\n' +
-  '*这是倾斜的文字*`\n\n' +
-  '***这是斜体加粗的文字***\n\n' +
-  '~~这是加删除线的文字~~ \n\n' +
-  '`console.log(Hello World)` \n\n' +
-  '```const a=2; ```' +
-  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
-  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
-  '**这是加粗的文字**\n\n' +
-  '*这是倾斜的文字*`\n\n' +
-  '***这是斜体加粗的文字***\n\n' +
-  '~~这是加删除线的文字~~ \n\n' +
-  '`console.log(Hello World)` \n\n' +
-  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
-  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
-  '**这是加粗的文字**\n\n' +
-  '*这是倾斜的文字*`\n\n' +
-  '***这是斜体加粗的文字***\n\n' +
-  '~~这是加删除线的文字~~ \n\n' +
-  '`console.log(Hello World)` \n\n' +
-  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
-  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
-  '**这是加粗的文字**\n\n' +
-  '*这是倾斜的文字*`\n\n' +
-  '***这是斜体加粗的文字***\n\n' +
-  '~~这是加删除线的文字~~ \n\n' +
-  '`console.log(Hello World)` \n\n' +
-  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
-  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
-  '**这是加粗的文字**\n\n' +
-  '*这是倾斜的文字*`\n\n' +
-  '***这是斜体加粗的文字***\n\n' +
-  '~~这是加删除线的文字~~ \n\n' +
-  '`console.log(Hello World)` \n\n';
+let markdown = '```jsx \n' + 'console \n' + '```\n' + '`ddddd`';
 
 const Header = () => {
   return (
@@ -52,7 +15,6 @@ const Header = () => {
           code: Code,
         }}
       />
-      <Code value="111" />
     </div>
   );
 };
--- components/Code.tsx
+++ components/Code.tsx
@@ -9,14 +9,12 @@ interface CodeProps {
 }
 
 const Code: React.FC<CodeProps> = ({ value }) => {
-  const codeString = '(num) => num + 1';
   useEffect(() => {
     SyntaxHighlighter.registerLanguage('javascript', js);
   }, []);
   return (
     <SyntaxHighlighter language="javascript" style={docco}>
       {value}
-      {codeString}
     </SyntaxHighlighter>
   );
 };
